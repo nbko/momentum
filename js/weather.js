af215c1305770cda30266be2e8f942f9bdaf6b36
nbko <nbko@uchicago.edu>
2023-11-06 19:11:33 -0600

    updating momentum

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = fe738b6decc87e414bc64f41047c3e0d; // API key not activated
+
+function onGeoLoc(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("Cant find you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoLoc, onGeoError);
